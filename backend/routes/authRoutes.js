bc63e2c6a70e7a4b673c63ba279bd8ca55be3d95
Nursultan <nursbastaubayev@gmail.com>
2025-04-01 18:21:07 +0600

    sad2222222

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,13 +1,7 @@
 // Auth routes
 const express = require('express');
 const router = express.Router();
-const { register, login, getTelegramUser } = require('../controllers/authController');
-
-// Register a new user
-router.post('/register', register);
-
-// Login a user
-router.post('/login', login);
+const { getTelegramUser } = require('../controllers/authController');
 
 // Verify Telegram user data
 router.post('/telegram', getTelegramUser);
